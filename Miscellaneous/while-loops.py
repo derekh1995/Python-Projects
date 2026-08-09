09f7622a43e63314ae2831eb1e4e9b6255ab7d09
Derek Holbrook <derekholbrook95@protonmail.com>
2026-08-09 09:20:28 -0400

    Add while loops for user input validation
    Created three different while loops.

---
+++ Miscellaneous/while-loops.py
@@ -0,0 +1,28 @@
+# While Loops
+# Executes some code while some condition remains true
+
+name = input("Enter your name:\n")
+
+# if name == "":
+#     print("You did not enter your name.")
+# else:
+#     print(f"Hello, {name}.")
+
+while name == "":
+    print("You did not enter your name.")
+    name = input("Enter your name:\n")
+print(f"Hello, {name}.")
+
+food = input("Enter a food you like (q to quit):\n")
+
+while not food == "q":
+    print(f"You like {food}")
+    food = input("Enter a food you like (q to quit):\n")
+print("Bye. Thanks for chatting.")
+
+num = int(input("Enter a number between 1-10:\n"))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid.")
+    num = int(input("Enter a number between 1-10:\n"))
+print(f"Your number is {num}.\n Thanks for choosing a valid number.")
