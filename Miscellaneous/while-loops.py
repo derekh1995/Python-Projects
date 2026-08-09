e6d09fc7d88384d464fc76ea5ae9d4a07de6ec4a
Derek Holbrook <derekholbrook95@protonmail.com>
2026-08-09 10:16:44 -0400

    Update while-loops.py
    Added try/except example

--- Miscellaneous/while-loops.py
+++ Miscellaneous/while-loops.py
@@ -26,3 +26,15 @@ while num < 1 or num > 10:
     print(f"{num} is not valid.")
     num = int(input("Enter a number between 1-10:\n"))
 print(f"Your number is {num}.\n Thanks for choosing a valid number.")
+
+##### try/except 
+
+honor = 0
+
+while honor < 0:
+    try:
+        honor = float(input("Enter character's honor level:\n))
+        if honor < 0:
+            print("Honor level cannot be less than zero.")
+    except ValueError:
+        print("Please enter a valid honor level")                        
