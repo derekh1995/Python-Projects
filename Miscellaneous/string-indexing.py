5221807f04604d374b3623ca91b11b228d2af71f
Derek Holbrook <derekholbrook95@protonmail.com>
2026-08-09 08:48:08 -0400

    Create string-indexing.py with indexing examples
    Add examples of string indexing in Python.

---
+++ Miscellaneous/string-indexing.py
@@ -0,0 +1,17 @@
+# String Indexing = accessing elements of a sequence using [] (indexing operator)
+# [start : end : step]
+
+# card = "1234-5678-9012-3456"
+
+# print(card[0:4])
+# print(card[5:9])
+# print(card[10:])
+# print(card[-1])
+# print(card[::2])
+# print(card[::-1])
+
+code1 = "qwkeYzvnfOmprdUktws bfghCjqlvAnprwNxzck tmbdDgwqfOlhvs rkpzTbnwqHfjtlIdxmrS"
+print(code1[4::5])
+
+code2 = "mmi4f9iOlpt1zL159Gdd0kIl54qvAfv3GfE76jV90Y15Iklvoas4fCUu"
+print(code2[6::7])
